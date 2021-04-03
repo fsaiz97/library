4629455a0e1a0213304b0f9795723d84ade1b456
fsaiz97 <75044237+fsaiz97@users.noreply.github.com>
2021-04-03 22:13:45 +0100

    Delete resources directory

--- resources/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import Resource
-
-
-# DataFlair
-class ResourceCreate(forms.ModelForm):
-    class Meta:
-        model = Resource
-        fields = '__all__'
