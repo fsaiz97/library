f564c1cca0f6b05f524d3ab113e1f4e6351a5717
fsaiz97 <75044237+fsaiz97@users.noreply.github.com>
2021-04-03 22:27:11 +0100

    Revert "Delete resources directory"
    This reverts commit 4629455a0e1a0213304b0f9795723d84ade1b456.

---
+++ resources/get_works.py
@@ -0,0 +1,129 @@
+# import required modules
+import requests
+#from pprint import pprint
+import json
+import pandas as pd
+from olclient.openlibrary import OpenLibrary
+from .models import *
+import datetime
+from dateutil import parser
+
+url_template = 'http://openlibrary.org/authors/{id}/works.json'
+
+
+# set template url for API call
+
+def get_works(name):
+    ol = OpenLibrary()
+    author_id = ol.Author.get_olid_by_name(name)
+
+    query_url = url_template.format(id=author_id)  # format API url to include OLID
+
+    resp = requests.get(query_url)  # use requests module to access API
+    if resp.ok:
+        response = resp.json()  # return json of results if ok
+    else:
+        return resp.reason  # print reason if not ok
+
+    datalist = response['entries']  # make json data easier to parse through
+
+    num_works = len(datalist)
+
+    author = [name] * num_works  # create list of author name (API call returns 50 results maximum)
+
+    titles = [entry['title'] for entry in datalist]  # create list of titles by parsing through json
+
+    revision = [entry['revision'] for entry in datalist]  # create list of revision number
+
+    key = [entry['key'] for entry in datalist]  # create list of keys
+
+    publish_date = []  # initialise list for publish date (not all entries have one)
+    for entry in datalist:
+        if 'first_publish_date' in entry.keys():
+            publish_date.append(entry['first_publish_date'])  # if publish date exists, append to list
+        else:
+            publish_date.append(None)  # if publish date does not exist, append 'N/A' to list
+
+    subjects = []  # initialise subject list
+    for entry in datalist:
+        if 'subjects' in entry.keys():
+            subjects.append(entry['subjects'])  # if subjects exist, append to list
+        else:
+            subjects.append('N/A')  # if subjects do not exist, append 'N/A' to list
+
+    places = []  # initialise places list
+    for entry in datalist:
+        if 'subject_places' in entry.keys():
+            places.append(entry['subject_places'])  # if places exist, append to list
+        else:
+            places.append('N/A')  # if places do not exist, append 'N/A' to list
+
+    characters = []  # initialise character list
+    for entry in datalist:
+        if 'subject_people' in entry.keys():
+            characters.append(entry['subject_people'])  # if characters exist, append to list
+        else:
+            characters.append('N/A')  # if characters do not exist, append 'N/A' to list
+
+    data = pd.DataFrame(
+        data={"Author": author, "Title": titles, "Revision": revision, "Key": key, "Publish Date": publish_date,
+              "Subjects": subjects, "Characters": characters, "Places": places})
+    # create pandas dataframe of lists, makes it easy to save as csv file
+    '''
+    name_list = name.split()
+    name = name_list[-1]
+
+    data.to_csv(name+".csv", sep=',', index = False)
+    #save author's data as csv file
+
+    with open(name+'.json', 'w') as OUTF:
+        json.dump(response, OUTF)
+	#create json dump of all returned data
+	'''
+
+    output = data.to_json(orient='records')
+
+    return output
+
+def save_works(works_json):
+    try:
+        author_name = works_json[0]["Author"]
+        author, created = Author.objects.get_or_create(name = author_name)
+        for work in works_json:
+            subjects = [] if work["Subjects"]=="N/A" else [Subject(subject_name=name) for name in work["Subjects"]]
+            subjects_new = []
+            for subject in subjects:
+                if subject not in Subject.objects.filter(subject_name=subject.subject_name):
+                    subject.save()
+                subjects_new.append(subject)
+            characters = [] if work["Characters"]=="N/A" else [Character(character_name=name) for name in work["Characters"]]
+            for character in characters:
+                if character not in Character.objects.filter(character_name=character.character_name):
+                    character.save()
+            places = [] if work["Places"]=="N/A" else [Place(place_name=name) for name in work["Places"]]
+            for place in places:
+                if place not in Place.objects.filter(place_name=place.place_name):
+                    place.save()
+            date = parser.parse(work["Publish Date"]) if work["Publish Date"] != None else None
+            if not Resource.objects.filter(title=work["Title"],author=author,revision=work["Revision"]):
+                book = Resource(
+                                title=work["Title"],
+                                author=author,
+                                revision=work["Revision"],
+                                publish_date=date,
+                                quantity=1,
+                                location=None)
+                #return book.title, book.author, book.revision, book.publish_date, book.quantity, book.location
+                book.save()
+                #return "yay"
+                ''' # Can't auto add subjects, characters or places at the moment
+                for subject in subjects_new:
+                    book.subjects.add(subject)
+                for character in characters:
+                    book.characters.add(character)
+                    for place in places:
+                        book.places.add(place)
+                '''
+        return 1
+    except:
+        return -1
---
+++ resources/urls.py
@@ -0,0 +1,27 @@
+from django.urls import path
+from . import views
+
+# from libraryapp.settings import DEBUG, STATIC_URL, STATIC_ROOT, MEDIA_URL, MEDIA_ROOT
+# from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('subject-list/', views.subjectList, name='subject-list'),
+    path('subject-create/', views.subjectCreate, name='subject-create'),
+    path('subject-delete/<str:pk>/', views.subjectDelete, name='subject-delete'),
+    path('character-list/', views.characterList, name='character-list'),
+    path('character-create/', views.characterCreate, name='character-create'),
+    path('character-delete/<str:pk>/', views.characterDelete, name='character-delete'),
+    path('place-list/', views.placeList, name='place-list'),
+    path('place-create/', views.placeCreate, name='place-create'),
+    path('place-delete/<str:pk>/', views.placeDelete, name='place-delete'),
+    path('location-list/', views.locationList, name='location-list'),
+    path('location-create/', views.locationCreate, name='location-create'),
+    path('location-delete/<str:pk>/', views.locationDelete, name='location-update'),
+    path('resource_api/', views.apiOverview, name='api'),
+    path('resource-list/', views.resourceList, name='resource-list'),
+    path('resource-detail/<str:pk>/', views.resourceDetail, name='resource-detail'),
+    path('resource-create/', views.resourceCreate, name='resource-create'),
+    path('resource-update/<str:pk>/', views.resourceUpdate, name='resource-update'),
+    path('resource-delete/<str:pk>/', views.resourceDelete, name='resource-delete'),
+]
